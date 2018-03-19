a1dc76458605e03af5d95f622918521478a5981f
Parul Priya <parulpriya@west-10-233-81-242.tamulink.tamu.edu>
2018-03-18 21:57:47 -0500

    Add include, make separate urls.py for accounts app

--- redditclone/urls.py
+++ redditclone/urls.py
@@ -13,14 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-import accounts.views
-
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^signup/', accounts.views.signup, name="signup"),
-    url(r'^login/', accounts.views.loginview, name="login"),
-
+    url(r'^accounts/', include('accounts.urls')),
 ]
