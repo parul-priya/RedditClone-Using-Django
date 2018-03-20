e75417a57f332a3d0f7b31d8b7797695033247f9
Parul Priya <parulpriya@west-10-232-103-2.tamulink.tamu.edu>
2018-03-19 23:08:52 -0500

    Add POST route for creating new posts

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from . import models
+from django.utils import timezone
 
 # Create your views here.
 @login_required
 def create(request) :
-	return render(request, 'posts/create.html')
+	if request.method == 'POST' :
+		if request.POST['title'] and request.POST['url'] :
+			post = models.Post()
+			post.title = request.POST['title']
+			post.url = request.POST['url']
+			post.date = timezone.datetime.now()
+			post.author = request.user
+			post.save()
+
+	else :
+		return render(request, 'posts/create.html')
